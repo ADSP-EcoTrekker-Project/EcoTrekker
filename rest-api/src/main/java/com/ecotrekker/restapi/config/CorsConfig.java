47bfafe448339fe94d438cc29ba76ca8637dbe80
erdem <erdem.arslan@tu-berlin.de>
2023-11-27 20:43:19 +0100

    update cors configuration

--- rest-api/src/main/java/com/ecotrekker/restapi/config/CorsConfig.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/v1/calc/co2")
                 .allowedOrigins("http://localhost:19006")
-                .allowedMethods("*");
+                .allowedMethods("POST");
     }
 
 }
