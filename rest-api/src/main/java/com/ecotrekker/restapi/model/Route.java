47c1a450bebb341dcff9e3d9722db41f43c1eabe
erdem <erdem.arslan@tu-berlin.de>
2023-12-09 12:40:11 +0100

    add rest-api validation constraints for RouteStep

--- rest-api/src/main/java/com/ecotrekker/restapi/model/Route.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/model/Route.java
@@ -3,10 +3,14 @@ package com.ecotrekker.restapi.model;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.validation.Valid;
 import lombok.Data;
 
 @Data
 public class Route {
+
+    @Valid
     private List<RouteStep> steps;
     private UUID id = UUID.randomUUID();
+
 }
--- rest-api/src/main/java/com/ecotrekker/restapi/model/RouteStep.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/model/RouteStep.java
@@ -1,11 +1,20 @@
 package com.ecotrekker.restapi.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class RouteStep {
+
+    @NotBlank(message = "start location must not be null or empty!")
     private String start;
+    @NotBlank(message = "end location must not be null or empty!")
     private String end;
+    @NotBlank(message = "vehicle must not be null or empty!")
     private String vehicle;
+    @Positive(message = "distance must be a positive number!")
     private Long distance;
+
 }
