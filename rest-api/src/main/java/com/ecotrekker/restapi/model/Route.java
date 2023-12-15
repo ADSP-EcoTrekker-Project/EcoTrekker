2d44f6ed705546d67cec15a3cae66b2b7c37d56d
erdem <erdem.arslan@tu-berlin.de>
2023-12-15 12:51:00 +0100

    fix issue with an empty but not null json and expecting at least one step in route

--- rest-api/src/main/java/com/ecotrekker/restapi/model/Route.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/model/Route.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.UUID;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Route {
 
     @Valid
+    @NotNull
+    @Size(min = 1, message = "At least one step is required in each route.")
     private List<RouteStep> steps;
     private UUID id = UUID.randomUUID();
 
