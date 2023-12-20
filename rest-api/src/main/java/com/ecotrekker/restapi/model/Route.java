313a2334796277b911f7a1b0c28555c72e31b0df
Leo Wilhelm Lierse <lierseleow@gmail.com>
2023-12-20 15:56:33 +0100

    Removed kafka WebClient to access co2-calculator service

--- rest-api/src/main/java/com/ecotrekker/restapi/model/Route.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/model/Route.java
@@ -3,10 +3,13 @@ package com.ecotrekker.restapi.model;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 public class Route {
@@ -14,7 +17,9 @@ public class Route {
     @Valid
     @NotNull(message = "Route must not be null")
     @Size(min = 1, message = "At least one step is required in each route.")
+    @Getter
     private List<RouteStep> steps;
+    
     private UUID id = UUID.randomUUID();
 
 }
--- rest-api/src/main/java/com/ecotrekker/restapi/service/EcotrekkerService.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/service/EcotrekkerService.java
@@ -1,48 +1,64 @@
 package com.ecotrekker.restapi.service;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.requestreply.RequestReplyFuture;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
+import org.springframework.web.reactive.function.client.WebClient.UriSpec;
 
+import com.ecotrekker.restapi.model.Route;
 import com.ecotrekker.restapi.model.RouteResult;
 import com.ecotrekker.restapi.model.Routes;
 import com.ecotrekker.restapi.model.RoutesResult;
-import com.ecotrekker.restapi.producer.EcotrekkerProducer;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EcotrekkerService {
 
-    private final EcotrekkerProducer producer;
-    private ObjectMapper objectMapper;
-
     @Autowired
-    public EcotrekkerService(EcotrekkerProducer producer, ObjectMapper objectMapper) {
-        this.producer = producer;
-        this.objectMapper = objectMapper;
+    private WebClient client;
+
+    @Value("${co2-calculator.timeout}")
+    private int timeout;
+
+    public Mono<RouteResult> getCo2(Route route){
+        UriSpec<RequestBodySpec> uriSpec = client.post();
+        RequestBodySpec bodySpec = uriSpec.uri("/v1/calc/co2");
+        RequestHeadersSpec<?> headersSpec = bodySpec.body(
+            Mono.just(route), Route.class);
+        ResponseSpec responseSpec = headersSpec.header(
+            HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML)
+            .retrieve();
+        Mono<RouteResult> response = responseSpec.bodyToMono(RouteResult.class);
+        return response;
     }
 
-    public String requestCalculation(Routes routes) throws JsonProcessingException {
-        HashMap<UUID, RequestReplyFuture<String, String, String>> futures = producer.sendMessages(routes);
-        CompletableFuture<Void> resultFuture = CompletableFuture.allOf(futures.values().toArray(new CompletableFuture[0]));
-        RoutesResult result = new RoutesResult();
-        LinkedList<RouteResult> routeResults = new LinkedList<>();
+    public Flux<RouteResult> fetchCO2(List<Route> routes){
+        return Flux.fromIterable(routes).flatMap(this::getCo2);
+    }
+
+    public RoutesResult requestCalculation(Routes routes) {
         try {
-            resultFuture.get(1, TimeUnit.SECONDS);
-            for(RequestReplyFuture<String, String, String> requestReply: futures.values()) {
-                routeResults.add(objectMapper.readValue(requestReply.get().value(), RouteResult.class));
-            }
+            Flux<RouteResult> fetchResults = fetchCO2(routes.getRoutes());
+            List<RouteResult> routeResultList = fetchResults.collectList().block(Duration.ofMillis(timeout));
+            RoutesResult result = new RoutesResult();
+            result.setRoutes(routeResultList);
+            return result;
         } catch (Exception e) {
-            // TODO what do we do here?
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
         }
-        result.setRoutes(routeResults);
-        return objectMapper.writeValueAsString(result);
     }
 }
