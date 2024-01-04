293a146b7f16ccdc687eb922addd7ed914e32606
Leo Wilhelm Lierse <lierseleow@gmail.com>
2024-01-04 15:29:45 +0100

    Add simple Exception Handler and Empty CORS config

---
+++ vehicle-consumption/src/main/java/com/ecotrekker/vehicleconsumption/config/cors/CorsConfig_C.java
@@ -0,0 +1,14 @@
+package com.ecotrekker.vehicleconsumption.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig_C implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    }
+    
+}
