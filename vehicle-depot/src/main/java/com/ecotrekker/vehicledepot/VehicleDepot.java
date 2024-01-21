3aebcc2bc17c12239004543da908cc66fff8f362
Leo Wilhelm Lierse <lierseleow@gmail.com>
2024-01-21 15:12:02 +0100

    Added skeleton for new depot service

---
+++ vehicle-depot/src/main/java/com/ecotrekker/vehicledepot/VehicleDepot.java
@@ -0,0 +1,13 @@
+package com.ecotrekker.vehicledepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDepot {
+
+    public static void main(String[] args) {
+		SpringApplication.run(VehicleDepot.class, args);
+	}
+    
+}
---
+++ vehicle-depot/src/main/java/com/ecotrekker/vehicledepot/controller/V1Controller.java
@@ -0,0 +1,10 @@
+package com.ecotrekker.vehicledepot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1")
+public class V1Controller {
+    
+}
