f7f639398c0159c2d8b2e8441b28be5713713229
Anders Hausding <ahausding@sensorstim.com>
2024-01-27 17:15:20 +0100

    feat: implement rest endpoint for route service

--- route-manager/src/main/java/com/ecotrekker/routemanager/controller/RouteManagerController.java
+++ route-manager/src/main/java/com/ecotrekker/routemanager/controller/RouteManagerController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecotrekker.routemanager.model.CalculationErrorResponse;
-import com.ecotrekker.routemanager.model.Route;
-import com.ecotrekker.routemanager.model.RouteResult;
+import com.ecotrekker.routemanager.model.RouteRequest;
+import com.ecotrekker.routemanager.model.RouteServiceException;
 import com.ecotrekker.routemanager.service.RouteService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -20,15 +20,15 @@ public class RouteManagerController {
     @Autowired
     private RouteService routeService;
 
-    @PostMapping("/calc/route")
-    public ResponseEntity<?> calculateCo2(@RequestBody Route route) throws JsonProcessingException {
-        RouteResult result = routeService.requestCalculation(route);
-        if (result == null) {
-            CalculationErrorResponse error = CalculationErrorResponse.builder().error("Invalid Route Data").build();
-            return ResponseEntity.status(400).body(error);
+    @PostMapping("/calc/routes")
+    public ResponseEntity<?> calculateRouteData(@RequestBody RouteRequest routeRequest) throws JsonProcessingException {
+        try {
+            return ResponseEntity.ok(routeService.requestCalculation(routeRequest));
+        } catch (RouteServiceException e) {
+            return ResponseEntity
+            .status(400)
+            .body(CalculationErrorResponse.builder().error(e.getMessage()).build());
         }
-        //TODO
-        return ResponseEntity.ok(result);
     }
 
 }
