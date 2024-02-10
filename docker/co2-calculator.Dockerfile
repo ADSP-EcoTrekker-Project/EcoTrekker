0d5b9a9f8bb2a3982269e231b8f93a62b8f3d904
Leo Wilhelm Lierse <lierseleow@gmail.com>
2024-02-10 14:56:29 +0100

    Fixed some Dockerfiles and added initial changes for the start script

--- docker/co2-calculator.Dockerfile
+++ docker/co2-calculator.Dockerfile
@@ -8,5 +8,7 @@ COPY --from=builder /ecotrekker/build/co2-calculator/build/libs/*.jar app.jar
 EXPOSE 8080
 
 ENV CONSUMPTION_SERVICE_ADDRESS=http://localhost:8085
+ENV GRID_CO2_SERVICE_ADDRESS=http://localhost:8086
+ENV VEHICLE_DEPOT_SERVICE_ADDRESS=http://localhost:8087
 
-ENTRYPOINT ["java", "-jar", "/app.jar", "--consumption-service.address=${CONSUMPTION_SERVICE_ADDRESS}"]
+ENTRYPOINT ["java", "-jar", "/app.jar", "--consumption-service.address=${CONSUMPTION_SERVICE_ADDRESS}", "--grid-co2-cache.address=${GRID_CO2_SERVICE_ADDRESS}", "--depot-service.address=${VEHICLE_DEPOT_SERVICE_ADDRESS}"]
--- setup/start.sh
+++ setup/start.sh
@@ -5,13 +5,32 @@ if [[ -z ${REPO} ]]; then
     echo "Set REPO Environment Variable to default value"
 fi
 
-docker run -d --restart unless-stopped -p 8082:8080 \
+docker run -d --restart unless-stopped -p 8087:8080 \
+    ${REPO}/eco-vehicle-depot:latest
+
+docker run -d --restart unless-stopped -p 8086:8080 \
+    ${REPO}/eco-grid-co2-cache:latest
+
+docker run -d --restart unless-stopped -p 8085:8080 \
     ${REPO}/eco-vehicle-consumption:latest
 
-docker run -d --restart unless-stopped -p 8081:8080 \
-    -e CONSUMPTION_SERVICE_ADDRESS="172.17.0.1:8082" \
+# Gamification here
+
+docker run -d --restart unless-stopped -p 8083:8080 \
+    -e VEHICLE_DEPOT_SERVICE_ADDRESS="172.17.0.1:8087" \
+    -e GRID_CO2_SERVICE_ADDRESS="172.17.0.1:8086" \
+    -e CONSUMPTION_SERVICE_ADDRESS="172.17.0.1:8085" \
     ${REPO}/eco-co2-calculator:latest
 
+docker run -d --restart unless-stopped -p 8084:8080 \
+    ${REPO}/eco-public-transport-distance:latest
+
+docker run -d --restart unless-stopped -p 8081:8080 \
+    -e DISTANCE_SERVICE_ADDRESS="172.17.0.1:8084" \
+    -e CO2_SERVICE_ADDRESS="172.17.0.1:8083" \
+    -e GAMIFICATION_SERVICE_ADDRESS="172.17.0.1:8082" \
+    ${REPO}/eco-route-manager:latest
+
 docker run -d --restart unless-stopped -p 8080:8080 \
-    -e CO2_CALCULATOR_ADDRESS="172.17.0.1:8081" \
+    -e ROUTE_SERVICE_ADDRESS="172.17.0.1:8081" \
     ${REPO}/eco-rest-api:latest
