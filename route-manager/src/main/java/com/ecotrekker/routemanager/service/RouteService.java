f52ffad109f60464ac92e6701fca470c8ceec0cc
Anders Hausding <ahausding@sensorstim.com>
2024-02-26 17:54:33 +0100

    chore: cleanup unused imports

--- route-manager/src/main/java/com/ecotrekker/routemanager/service/RouteService.java
+++ route-manager/src/main/java/com/ecotrekker/routemanager/service/RouteService.java
@@ -1,36 +1,25 @@
 package com.ecotrekker.routemanager.service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ecotrekker.routemanager.clients.Co2ServiceClient;
 import com.ecotrekker.routemanager.clients.DistanceServiceClient;
 import com.ecotrekker.routemanager.clients.GamificationServiceClient;
-import com.ecotrekker.routemanager.model.DistanceReply;
 import com.ecotrekker.routemanager.model.DistanceRequest;
-import com.ecotrekker.routemanager.model.GamificationReply;
 import com.ecotrekker.routemanager.model.GamificationRequest;
-import com.ecotrekker.routemanager.model.RouteServiceException;
 import com.ecotrekker.routemanager.model.RouteResult;
 import com.ecotrekker.routemanager.model.RouteStep;
 import com.ecotrekker.routemanager.model.RouteStepResult;
 import com.ecotrekker.routemanager.model.RoutesRequest;
 import com.ecotrekker.routemanager.model.RoutesResult;
 
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import org.springframework.beans.factory.annotation.Value;
 
-@Slf4j
 @Service
 public class RouteService {
     
