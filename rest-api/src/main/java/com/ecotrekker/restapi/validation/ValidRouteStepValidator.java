8152b3a867519342a28055746cf09775342286ac
erdemarslan <erdem.arslan8@hotmail.com>
2024-03-17 20:44:31 +0100

    validation of the line parameter

--- rest-api/src/main/java/com/ecotrekker/restapi/validation/ValidRouteStepValidator.java
+++ rest-api/src/main/java/com/ecotrekker/restapi/validation/ValidRouteStepValidator.java
@@ -5,19 +5,32 @@ import com.ecotrekker.restapi.model.RouteStep;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ValidRouteStepValidator implements ConstraintValidator<ValidRouteStep, RouteStep> {
+
+    private static final List<String> lineVehicles = Arrays.asList("bus", "e-bus", "ice-bus", "metro", "tram", "train");
 
     @Override
     public void initialize(ValidRouteStep constraintAnnotation) {
     }
 
     public boolean isBlank(String string){
-        return string == null || string.trim().length() < 0;
+        return string == null || string.trim().isEmpty();
     }
 
     @Override
     public boolean isValid(RouteStep value, ConstraintValidatorContext context) {
         if ((isBlank(value.getStart()) && isBlank(value.getEnd())) && value.getDistance() == null) {
+            context.buildConstraintViolationWithTemplate("Either start and end or distance must be provided!")
+                    .addConstraintViolation();
+            return false;
+        }
+
+        if (lineVehicles.contains(value.getVehicle()) && value.getDistance() == null && isBlank(value.getLine())) {
+            context.buildConstraintViolationWithTemplate("For this vehicle either distance or the line must be provided!")
+                    .addConstraintViolation();
             return false;
         }
 
@@ -26,8 +39,7 @@ public class ValidRouteStepValidator implements ConstraintValidator<ValidRouteSt
             return false;
         }
 
-        context.buildConstraintViolationWithTemplate("Either start and end or distance must be provided!")
-                .addConstraintViolation();
+
         return true;
     }
 
