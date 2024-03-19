726a00c32701a904dca65e65d3e2b5a6bac10610
RichardDetlefs <richard.detlefs@outlook.de>
2024-03-19 14:46:20 +0100

    Updated V1ControllerTest.java.

--- vehicle-consumption/src/test/java/com/ecotrekker/vehicleconsumption/Endpoints/V1ControllerTest.java
+++ vehicle-consumption/src/test/java/com/ecotrekker/vehicleconsumption/Endpoints/V1ControllerTest.java
@@ -1,6 +1,7 @@
 package com.ecotrekker.vehicleconsumption.Endpoints;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.ecotrekker.vehicleconsumption.messages.VehicleConsumptionReply;
 import com.ecotrekker.vehicleconsumption.messages.VehicleConsumptionRequest;
@@ -22,12 +25,51 @@ public class V1ControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    void testConsumptionEndpoint() throws Exception {
+    void testConsumptionEndpoint() {
         VehicleConsumptionRequest request = new VehicleConsumptionRequest("car");
         VehicleConsumptionReply reply = this.restTemplate.postForObject(
             "http://localhost:"  + port + "/v1/consumption",
             request, 
             VehicleConsumptionReply.class);
-        assertTrue(reply.getVehicleName().compareTo(request.getVehicleName()) == 0);
+        assertEquals(0, reply.getVehicleName().compareTo(request.getVehicleName()));
+    }
+
+    @Test
+    void testConsumptionEndpointWithCar() {
+        VehicleConsumptionRequest request = new VehicleConsumptionRequest("car");
+
+        ResponseEntity<VehicleConsumptionReply> response = restTemplate.postForEntity(
+                "http://localhost:"  + port + "/v1/consumption",
+                request,
+                VehicleConsumptionReply.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        VehicleConsumptionReply reply = response.getBody();
+        assertNotNull(reply);
+        assertEquals("car", reply.getVehicleName());
+    }
+
+    @Test
+    void testConsumptionEndpointWithInvalidVehicle() {
+        VehicleConsumptionRequest request = new VehicleConsumptionRequest("invalid_vehicle");
+
+        ResponseEntity<VehicleConsumptionReply> response = restTemplate.postForEntity(
+                "http://localhost:"  + port + "/v1/consumption",
+                request,
+                VehicleConsumptionReply.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void testConsumptionEndpointWithEmptyVehicle() {
+        VehicleConsumptionRequest request = new VehicleConsumptionRequest("");
+
+        ResponseEntity<VehicleConsumptionReply> response = restTemplate.postForEntity(
+                "http://localhost:"  + port + "/v1/consumption",
+                request,
+                VehicleConsumptionReply.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 }
