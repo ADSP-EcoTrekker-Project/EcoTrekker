6e37f4ce2a2fa741dbda3a2fba3bcfd857e6dbf4
Leo Wilhelm Lierse <lierseleow@gmail.com>
2024-03-22 16:40:02 +0100

    Deleted unused services

--- grid-cache/locustfile.py
+++
@@ -1,7 +0,0 @@
-from locust import HttpUser, task
-
-class EcoTrekkerUser(HttpUser):
-
-    @task
-    def request(self):
-        self.client.get("/v1/grid")
--- grid-cache/src/main/java/com/ecotrekker/gridco2cache/messages/IntensityResponse.java
+++
@@ -1,8 +0,0 @@
-package com.ecotrekker.gridco2cache.messages;
-
-import lombok.Data;
-
-@Data
-public class IntensityResponse {
-    private Double carbonIntensity;
-}
